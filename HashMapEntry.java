7bfdae3ceea053a7cd39fea8ae926c1c5985fa57
Eric Osterman <96360406+OstermanEric@users.noreply.github.com>
2022-08-04 11:43:43 -0400

    HashMap Entry class utilized in HashMap class.

---
+++ HashMapEntry.java
@@ -0,0 +1,35 @@
+/**
+ * name: Eric Osterman
+ * Assignment: ALA9
+ * assistance from Prof.Urban
+*/
+
+public class HashMapEntry<K, V> {
+	private K key;
+	private V value;
+
+	public HashMapEntry(K k, V v) {
+		key = k;
+		value = v;
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+	public V getValue() {
+		return value;
+	}
+
+	public void setKey(K k) {
+		key = k;
+	}
+
+	public void setValue(V v) {
+		value = v;
+	}
+
+	public String toString() {
+		return "(" + key + ", " + value + ")";
+	}
+}
